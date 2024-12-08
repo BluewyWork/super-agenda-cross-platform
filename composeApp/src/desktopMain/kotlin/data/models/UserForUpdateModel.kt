df4c6d0049453565c9d6921076b51efc9653bf68
BluewyWork <bluewy.s.work@gmail.com>
2024-12-08 23:08:34 +0100

    feat: update

--- composeApp/src/desktopMain/kotlin/data/models/UserForUpdateModel.kt
+++ composeApp/src/desktopMain/kotlin/data/models/UserForUpdateModel.kt
@@ -2,17 +2,18 @@ package data.models
 
 import domain.models.Membership
 import domain.models.UserForUpdate
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class UserForUpdateModel(
    val username: String?,
-   val membershipModel: MembershipModel?
+   @SerialName(value = "membership") val membershipModel: MembershipModel?
 )
 
 fun UserForUpdate.toData() = UserForUpdateModel(
    username = username,
-   membershipModel = when(membership) {
+   membershipModel = when (membership) {
       Membership.FREE -> MembershipModel.FREE
       Membership.PREMIUM -> MembershipModel.PREMIUM
       null -> null
